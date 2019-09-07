9138cf47227919adc2b908d576e407bfecd9f526
yulibaozi <527198700@qq.com>
2019-09-07 17:58:54 +0800

    Create example_test.go

---
+++ example_test.go
@@ -0,0 +1,61 @@
+package nginxconf_test
+
+import (
+	"fmt"
+
+	"github.com/caas-one/nginxconf"
+	"github.com/caas-one/nginxconf/core"
+)
+
+func Example_nginxconf_Parse() {
+	path := "./nginx.conf"
+	global, err := nginxconf.Parse(path)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(global)
+}
+
+func Example_nginxconf_RenderToString() {
+	path := "./nginx.conf"
+	global, err := nginxconf.Parse(path)
+	if err != nil {
+		panic(err)
+	}
+	result, err := nginxconf.RenderToString(global)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result)
+}
+
+func Example_RenderServer() {
+	server := &core.Server{
+		Listen:    "80",
+		Name:      "eg.com",
+		ErrorPage: "500 502 503 504 /index.html",
+		Locations: []core.Location{
+			core.Location{
+				Path:                 "/",
+				ClientBodyBufferSize: "128k",
+				ClientMaxBodySize:    "10m",
+				ProxyRedirect:        "off",
+				IfBlocks: []core.LocationIfBlock{
+					core.LocationIfBlock{
+						Condition: "$request_uri ~* /zgt/",
+						ProxyPass: "http://eg.com/ortal_http",
+					},
+				},
+			},
+			core.Location{
+				Path: "= /index.html",
+				Root: "/eg/500",
+			},
+		},
+	}
+	result, err := nginxconf.RenderToString(server)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(result)
+}
